db3ef9ae3182e3b7393f22c0e71612a73ba95c23
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-07-08 17:14:13 +0200

    fixed navbar+contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import Contactimg from '../assets/contact-image.jpg'
+import React from "react";
+import { FaFacebookF, FaGooglePlusG, FaTwitter } from "react-icons/fa";
+import Contactimg from "../assets/contact-image.jpg";
 const Contact = () => {
   return (
-    <div className='contact'>
-      <div className='Image'>
-        <img src={Contactimg} />
-        
-        
+    <div className="contact">
+   
+      <div className="container">
+        <div className="content">
+        <div className="ok">
+      <h1>hello</h1>
+    </div>
+          <div className="col-1">
+            <div className="image">
+              <img src={Contactimg} alt="/" />
+            </div>
+          </div>
+
+          <div className="col-2">
+            <div className="para">
+              <p>my name is Athenkosi Vinqi,</p>
+              <p>and I am currently watching</p>
+              <p>a series called Stranger Things</p>
+            </div>
+
+            <div className="para">
+              <p>you can reach me on:</p>
+              <p>tel: 073 390 1637</p>
+              <p>email: athenkosivinqi25@gmail.com</p>
+            </div>
+
+            <div className="para">
+              <p>
+                share: <FaFacebookF className="spacing"/> <FaTwitter className="spacing"/> <FaGooglePlusG className="spacing"/>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
