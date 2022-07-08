98e7c977b46ee7e290fe8ac156e097a30a95caa9
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-07-08 13:34:48 +0200

    navbar+contact page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Aglet from "../assets/aglet_logo.svg";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={Aglet} alt="/" />
+          </Link>
+        </div>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <Link to="/">
+            <li className="nav-item" onClick={handleClick}>
+              home
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li className="nav-item" onClick={handleClick}>
+              contact
+            </li>
+          </Link>
+        </ul>
+
+        <div className="toggler" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={20} style={{ color: "#000" }} />
+          ) : (
+            <FaBars size={20} />
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Contactimg from '../assets/contact-image.jpg'
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className='Image'>
+        <img src={Contactimg} />
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Contact
